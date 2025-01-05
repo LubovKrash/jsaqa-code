92f16488a21643643ed19a61aa29d6434f9e4dfc
LubovKrasheninnikova <altukhovalv@yandex.ru>
2025-01-05 21:13:07 +0300

    task 1 completed

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -15,3 +15,25 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+
+it("should return an empty array for empty input", () => {
+  expect(sorting.sortByName([])).toEqual([]);
+});
+
+it("should return the same array if there is only one element", () => {
+  expect(sorting.sortByName(["Гарри Поттер"])).toEqual(["Гарри Поттер"]);
+});
+
+it("should return the same array if all elements are identical", () => {
+  expect(
+    sorting.sortByName(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"])
+  ).toEqual(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"]);
+});
+
+it("should correctly sort strings with special characters", () => {
+  expect(sorting.sortByName(["apple!", "apple?", "apple."])).toEqual([
+    "apple!",
+    "apple.",
+    "apple?",
+  ]);
+});
